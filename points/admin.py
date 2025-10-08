a727178a75e3ac7b60ff04e908cd32f37b68154b
Mathieu Cazenave <nunespace@gmail.com>
2025-10-08 18:23:32 +0200

    modif admin : filtre par famille

--- points/admin.py
+++ points/admin.py
@@ -3,8 +3,31 @@ from django.contrib import admin
 from .models import PointNegatif, PointPositif, BaremeRecompense, BaremePointPositif, BaremePointNegatif
 
 
-admin.site.register(PointPositif)
-admin.site.register(PointNegatif)
-admin.site.register(BaremeRecompense)
-admin.site.register(BaremePointPositif)
-admin.site.register(BaremePointNegatif)
+# filtrer par famille
+class BaremeRecompenseAdmin(admin.ModelAdmin):
+    list_filter = ('famille',)
+
+
+class BaremePointPositifAdmin(admin.ModelAdmin):
+    list_filter = ('famille',)
+
+
+class BaremePointNegatifAdmin(admin.ModelAdmin):
+    list_filter = ('famille',)
+
+# Filtrer par famille dans l'admin
+
+
+class PointPositifAdmin(admin.ModelAdmin):
+    list_filter = ('enfant__famille',)
+
+
+class PointNegatifAdmin(admin.ModelAdmin):
+    list_filter = ('enfant__famille',)
+
+
+admin.site.register(PointPositif, PointPositifAdmin)
+admin.site.register(PointNegatif, PointNegatifAdmin)
+admin.site.register(BaremeRecompense, BaremeRecompenseAdmin)
+admin.site.register(BaremePointPositif, BaremePointPositifAdmin)
+admin.site.register(BaremePointNegatif, BaremePointNegatifAdmin)
