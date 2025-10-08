0e6413f08a6af7511b7ed82fada6c5dbd1db96c4
Mathieu Cazenave <nunespace@gmail.com>
2025-10-08 17:19:21 +0200

    maj admin

--- points/admin.py
+++ points/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import PointNegatif, PointPositif
+from .models import PointNegatif, PointPositif, BaremeRecompense, BaremePointPositif, BaremePointNegatif
 
 
 admin.site.register(PointPositif)
 admin.site.register(PointNegatif)
+admin.site.register(BaremeRecompense)
+admin.site.register(BaremePointPositif)
+admin.site.register(BaremePointNegatif)
