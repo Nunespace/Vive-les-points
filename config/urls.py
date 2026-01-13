b592784ab4ef111ace5f35aeed85fe19899f0fe5
Mathieu Cazenave <nunespace@gmail.com>
2026-01-13 15:26:21 +0100

    ajour redirections pour corrections google serach

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.urls import include, path
 from famille.views import landing_view, confidentialite_view
 from django.shortcuts import render
+from django.views.generic import RedirectView
 
 
 def maintenance(request):
@@ -17,6 +18,9 @@ urlpatterns = [
     path("famille/", include("famille.urls")),
     path("points/", include("points.urls")),
     path("admin/", admin.site.urls),
+    # Redirections pour les anciennes URLs
+    path("login/", RedirectView.as_view(url="/famille/login/",permanent=True)),
+    path("inscription/", RedirectView.as_view(url="/famille/inscription/",permanent=True),),
 ]
 
 if settings.DEBUG:
