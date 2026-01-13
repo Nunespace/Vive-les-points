7124e67030de9e7e399482b8093e71b1a7ea00ae
Mathieu Cazenave <nunespace@gmail.com>
2026-01-13 15:38:37 +0100

    Suppresion de SessionDebugMiddleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'famille.middleware.SessionDebugMiddleware',
 ]
 
 ROOT_URLCONF = "config.urls"
